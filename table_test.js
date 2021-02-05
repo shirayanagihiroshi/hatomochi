e213d0104590f9c2d6a6897f87591a865483fe9d
hiroshi <shirayanagi.hiroshi@gmail.com>
2021-02-05 18:24:52 +0900

    add click

--- table_test.js
+++ table_test.js
@@ -1,3 +1,13 @@
-function table_test_start () {
-  console.log('aa');
-}
+$(function() {
+
+  function table_test_start () {
+    console.log('now test start');
+  }
+
+  $('.testtable').on('click', function (event) {
+    console.log('click!');
+  });
+
+  $(window).on('ready', table_test_start());
+
+});
