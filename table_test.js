16516fd775ef26e2a030bc59df1a58e331cd7f01
shirayanagihirosh <shirayanagi.hiroshi@gmail.com>
2021-02-15 15:28:40 +0900

    当たり前に分かるところの計算まで実装

--- table_test.js
+++ table_test.js
@@ -6,7 +6,7 @@ $(function() {
   for (i=0; i<9; i++) {
     sudoku[i] = new Array(9);
     for (j=0; j<9; j++) {
-      sudoku[i][j] = kouho.slice();
+      //sudoku[i][j] = kouho.slice();
     }
   }
 
@@ -36,39 +36,44 @@ $(function() {
 
     for (i=0; i<9; i++) {
       for (j=0; j<9; j++) {
-        //１桁の整数なら
-        if (0 < world[i].children[j].textContent  && world[i].children[j].textContent < 10){
-//          console.log(world[i].children[j].nodeValue);
-          console.log('i:'+i+'j:'+j+','+world[i].children[j].textContent);
-//        } else {
-//          console.log('i:'+i+'j:'+j+','+world[i].children[j].textContent);
+        //１桁の整数なら、その結成済の値として保存
+        if (0 < world[i+1].children[j+1].textContent  && world[i+1].children[j+1].textContent < 10){
+          sudoku[i][j] = world[i+1].children[j+1].textContent;
+        // そうでなければ、候補のリストを設定
+        } else {
+          sudoku[i][j] = kouho.slice();
+          // 「候補」属亭を設定
+          world[i+1].children[j+1].setAttribute('kouho', 't');
         }
       }
     }
 
-    console.log(sudoku[0].length);
-    sudoku[0]=100;
-    console.log(sudoku[0]);
-    sudoku[1]=101;
-    console.log(sudoku[1]);
-    sudoku[2]=102;
-    console.log(sudoku[2]);
-    sudoku[3][0]=103;
-    console.log(sudoku[3][0]);
-    sudoku.unshift(5);//リストの先頭に追加
-    sudoku.push(104);//リストの最後に追加
-    console.log(sudoku);
-    //sudoku.splice(3,1);
-    remove_from_list(sudoku, 102);
-    console.log(sudoku);
-    console.log(sudoku.length);
-    console.log('Array.isArray(sudoku):' + Array.isArray(sudoku));
-    console.log('Array.isArray([1,2]]):' + Array.isArray([1,2]));
-    console.log('Array.isArray(1]):' + Array.isArray(1));
+    for (i=0; i<9; i++) {
+      for (j=0; j<9; j++) {
+        //候補のリストが無いところは、すなわち決定済の値である
+        if (!Array.isArray(sudoku[i][j])) {
+          //決定済の値をたよりに候補を削ってゆく
+          remove_ketteisumi(sudoku, sudoku[i][j], i, j);
+        }
+      }
+    }
 
-    console.log(world[2].children[0].textContent);
-    for (i=1; i<10 ;i++) {
-      world[i].children[5].textContent = sudoku[4][2];//'hoge'+i
+//    sudoku.unshift(5);//リストの先頭に追加
+//    sudoku.push(104);//リストの最後に追加
+//    console.log(sudoku);
+
+    for (i=0; i<9; i++) {
+      for (j=0; j<9; j++) {
+        if (world[i+1].children[j+1].hasAttribute('kouho')){
+          //候補が1つしかないときは決定とする
+          if (sudoku[i][j].length == 1) {
+            world[i+1].children[j+1].textContent = sudoku[i][j][0];
+            world[i+1].children[j+1].removeAttribute('kouho');
+          } else {
+            world[i+1].children[j+1].textContent = sudoku[i][j];
+          }
+        }
+      }
     }
   });
 
@@ -78,37 +83,75 @@ function table_test_start () {
   console.log('now test start');
 }
 
+// セルの数値が決定済のところを頼りに候補のリストから不要なものを削除する。
+//
 //sudoku: 9×9のマスが候補のリストを一つづつ持ったもの
 //num   : 確定済のセルの値
-//i     : 確定済のセルの位置
-//j     : 確定済のセルの位置
-function remove_from_yoko (sudoku, num, i, j) {
-  var s;
+//i     : 確定済のセルの位置(0～8)
+//j     : 確定済のセルの位置(0～8)
+function remove_ketteisumi (sudoku, num, i, j) {
+  remove_from_yoko(sudoku, num, i, j);
+  remove_from_tate(sudoku, num, i, j);
+  remove_from_kukei(sudoku, num, i, j);
+}
 
-  for (s=0; s<9; s++) {
-    if (s != j && Array.isArray(sudoku[i][s])){
-      remove_from_list(sudoku[i][s], num);
+function remove_from_yoko (sudoku, num, i, j) {
+  var t;
+
+  for (t = 0; t < 9; t++) {
+    if (t != j && Array.isArray(sudoku[i][t])){
+      remove_from_list(sudoku[i][t], num);
     }
   }
 }
 
-//sudoku: 9×9のマスが候補のリストを一つづつ持ったもの
-//num   : 確定済のセルの値
-//i     : 確定済のセルの位置
-//j     : 確定済のセルの位置
 function remove_from_tate (sudoku, num, i, j) {
   var s;
 
-  for (s=0; s<9; s++) {
+  for (s = 0; s < 9; s++) {
     if (s != i && Array.isArray(sudoku[s][j])){
       remove_from_list(sudoku[s][j], num);
     }
   }
 }
 
+function remove_from_kukei (sudoku, num, i, j) {
+  var s, t, tate_start, tate_end, yoko_start, yoko_end;
+
+  if (i < 3) {
+    tate_start = 0;
+    tate_end   = 3;
+  } else if (i < 6) {
+    tate_start = 3;
+    tate_end   = 6;
+  } else {
+    tate_start = 6;
+    tate_end   = 9;
+  }
+
+  if (j < 3) {
+    yoko_start = 0;
+    yoko_end   = 3;
+  } else if (j < 6) {
+    yoko_start = 3;
+    yoko_end   = 6;
+  } else {
+    yoko_start = 6;
+    yoko_end   = 9;
+  }
+
+  for (s = tate_start; s < tate_end; s++) {
+    for (t = yoko_start; t < yoko_end; t++) {
+      if ( !(s == i && t == j) && Array.isArray(sudoku[s][t])){
+        remove_from_list(sudoku[s][t], num);
+      }
+    }
+  }
+}
+
 // arrにnumがあればそれを削除する
 function remove_from_list (arr, num) {
-  var index = arr.indexOf(num);
+  var index = arr.indexOf(Number(num)); //Number()がないと文字扱いでhitしなかった
   if (index != -1) {
     //numのある位置から1個の要素を削除
     arr.splice(index,1);
